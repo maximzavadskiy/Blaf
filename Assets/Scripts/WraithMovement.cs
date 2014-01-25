a9b584cbecf353286cf31d39420ae44ceae180e3
Markku Velinen <dzarkki@hotmail.com>
2014-01-25 23:02:20 +0200

    Added bunch of new stuff (wraith, platforms) and fixed scene 3

---
+++ Assets/Scripts/WraithMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WraithMovement : MonoBehaviour {
+
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("hero");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 newPosition = transform.position;
+		newPosition.y += (player.transform.position.y > newPosition.y ? 1 : -1) * 0.03f;
+
+		float xMove = (player.transform.position.x > newPosition.x ? 1 : -1) * 0.05f;
+
+		// Stay away
+		if (Mathf.Abs(player.transform.position.y - newPosition.y) > 1)
+		{
+			// Go further away
+			if (Mathf.Abs(player.transform.position.x - newPosition.x) < 10)
+				newPosition.x -= xMove;
+
+			GetComponent<CircleCollider2D>().enabled = false;
+		}
+		else
+		{
+			// Chase
+			newPosition.x += xMove;
+
+			GetComponent<CircleCollider2D>().enabled = true;
+		}
+		transform.position = newPosition;
+	}
+}
