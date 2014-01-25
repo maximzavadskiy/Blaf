b1f4df915f2fae282eb017184f6eb419ef1ef6f4
Markku Velinen <dzarkki@hotmail.com>
2014-01-25 16:40:17 +0200

    Changed jumping height to lower, Added star background

---
+++ Assets/Scripts/StarBackgroundMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarBackgroundMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xOffset = -transform.parent.transform.position.x * 0.03f;
+		float yOffset = -transform.parent.transform.position.y * 0.03f + 5;
+		Vector3 newPos = new Vector3 (xOffset, yOffset, transform.localPosition.z);
+		transform.localPosition = newPos;
+	}
+}
