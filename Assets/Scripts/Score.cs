d22e71974ecc342c6d1e62bdb0902bbd1a667269
Parseri <niemi.aj@gmail.com>
2014-01-26 12:12:53 +0200

    score and other bug fixing

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,18 +7,25 @@ public class Score : MonoBehaviour {
 
 	public int kittenSaved = 0;
 	public int kittenKilled = 0;
+	private int totalKittens;
 
 	//singleton, resets every level
 
 	public static Score instance;
 
 	// Use this for initialization
-	void Awake () {
+	void Awake () 
+	{
 		instance = this;
 	}
-	
+
+	void Start()
+	{
+		totalKittens = GameObject.FindGameObjectsWithTag("Kitten").GetLength(0);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		guiText.text = "Kills: " + kills.ToString () + "\nKitten saved :" + kittenSaved + "\nKitten lost :" + kittenKilled;
+		guiText.text = "Kills: " + kills.ToString () + "\nKitten saved: " + kittenSaved + "/" + totalKittens;
 	}
 }
