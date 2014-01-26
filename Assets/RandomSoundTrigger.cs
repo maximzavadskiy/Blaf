d02396223f2329518bc4ee459c55deb0c3f35c51
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2014-01-26 12:43:35 +0200

    Monster far and close sounds - in prefab. Creepy Sounds triggers. My level.

---
+++ Assets/RandomSoundTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSoundTrigger : MonoBehaviour {
+
+	public AudioClip[] sounds;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag ("Player")) {
+			if(sounds.Length > 0 ) AudioSource.PlayClipAtPoint( sounds[Random.Range(0,sounds.Length - 1 )], transform.position, 100f );
+			Destroy(gameObject);
+		}
+	}
+}
